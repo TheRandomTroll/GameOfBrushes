ab8cd64290a0b8bb8cf60ea5fdd5756fbaeda933
Mihail Gerginov <Mihail Gerginov>
2017-03-25 12:36:32 +0200

    Initial commit

---
+++ GameOfBrushes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameOfBrushes.Startup))]
+namespace GameOfBrushes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
