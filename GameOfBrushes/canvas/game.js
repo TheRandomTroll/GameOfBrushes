5676208e4742fd5072e4c3bc3bb03d1cce6afe39
Pejo-306 <penikolov23@gmail.com>
2017-03-25 18:28:52 +0200

    Add files via upload
    Working color randomizer.

--- GameOfBrushes/canvas/game.js
+++ GameOfBrushes/canvas/game.js
@@ -62,3 +62,38 @@ function mouseMovement(event) {
     document.getElementById('mouse').innerHTML =
         "Mouse X: " + mouseX + ", Mouse Y: " + mouseY;
 }
+
+function Randomizer(timeDelta) {        
+    this.start = Number(new Date());  // Put the server time here !!!
+
+    this.timeDelta = timeDelta; // in miliseconds
+    
+    /*
+    this.start = function() {
+        setTimeout(this.randomizeColor, this.timeDelta);
+    }
+    */
+
+    this.randomizeColor = function() {
+        // generate three random numbers (0-255) and convert them to hexadecimal
+        var red = Math.round(Math.random() * 255).toString(16);
+        var green = Math.round(Math.random() * 255).toString(16);
+        var blue = Math.round(Math.random() * 255).toString(16);
+        
+        console.log(red, red.length, parseInt(red, 16));
+        console.log(green, green.length, parseInt(green, 16));
+        console.log(blue, blue.length, parseInt(blue, 16));
+        
+        // generate the final color (in hexadecimal)
+        var colors = [red, green, blue];
+        var color = "#";
+        for(var i = 0; i < colors.length; i++) {
+            if(colors[i].length == 1) {
+                colors[i] = "0" + colors[i]
+            }
+            color += colors[i];
+        }
+        console.log(color);
+    }
+}
+            
