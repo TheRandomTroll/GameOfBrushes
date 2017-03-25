3985958b24e40f3045533d08e6828ec68853c685
Pejo-306 <penikolov23@gmail.com>
2017-03-25 15:22:23 +0200

    Add files via upload
    Кодът е изчистен. Canvas-ът рабои.

--- GameOfBrushes/canvas/game.js
+++ GameOfBrushes/canvas/game.js
@@ -1,26 +1,46 @@
 function Canvas(id) {
     this.canvas = document.getElementById(id);
     this.context = this.canvas.getContext('2d');
-}
 
-function drawOnCanvas(event, canvas) {
-    // Note: the canvas parameter here is the Canvas object, not the html element    
-    mousePosition(event);
-    
-    if(paint) {
-        // convert the mouse coordinates on the screen to the canvas coordinates
-        var coefficientX = canvas.canvas.width / Number(style.getPropertyValue('width').slice(0, 3));
-        var canvasX = coefficientX * mouseX - canvas.canvas.offsetLeft / 2;
-        var coefficientY = canvas.canvas.height / Number(style.getPropertyValue('height').slice(0, 3));
-        var canvasY = coefficientY * mouseY - canvas.canvas.offsetTop / 2;
+    var id = this.context.createImageData(1, 1);
+    var d = id.data;
+    d[0] = 255; // red
+    d[1] = 0; // green
+    d[2] = 0; // blue
+    d[3] = 255; // alpha
 
-        canvas.context.putImageData(id, canvasX, canvasY);
+    this.drawOnCanvas = function (event) {
+        // Note: the canvas parameter here is the Canvas object, not the html element    
+        if(paint) {
+            setMousePosition(event);
+            var canvasX = mouseX - this.canvas.offsetLeft;
+            var canvasY = mouseY - this.canvas.offsetTop;
+            this.context.putImageData(id, canvasX, canvasY);
+        }    
     }
 }
 
-function mousePosition(event) {
+var mouseX, mouseY;
+function setMousePosition(event) {
+    /* Set mouse coordinates */
     mouseX = event.clientX;
     mouseY = event.clientY;
+}
+
+var paint;
+function drawCond(event) {
+    /* Start or stop painting on click */
+    if(event) {
+        switch (paint) {
+            case true: // stop painting
+                paint = false;
+                break;
+        
+            default: // start painting (if paint in (undefined, false))
+                paint = true;
+                break;
+        }
+    }
 }
 
 function mouseMovement(event) {
